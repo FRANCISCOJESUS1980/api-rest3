026a5a004c68c9945a8be9b1833f30a4819f5b8e
FRANCISCOJESUS1980 <jesusgonzalezvergara@hotmail.com>
2024-09-07 12:50:28 +0200

    feat metiendo character y movies modelos

---
+++ src/api/models/movie.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const movieSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, trim: true },
+    duration: { type: Number, required: true },
+    categories: [{ type: String, enum: ['terror', 'comedia', 'misterio'] }],
+    img: { type: String, required: true }
+  },
+  {
+    timestamps: true
+  }
+)
+
+const Movie = mongoose.model('movies', movieSchema, 'movies')
+module.exports = Movie
